c3aa3090ac1300eba91f0833d14a6775e55bc71c
Saulo Falcão <saulofalcao@gmail.com>
2013-11-17 22:57:02 -0500

    Adicionando Controller de Páginas Estáticas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ControleDeEstoque::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/ajuda"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
