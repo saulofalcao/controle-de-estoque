903cb5165053e4e4a1a500a6ede29d1517216776
Saulo Falcão <saulofalcao@gmail.com>
2013-11-23 23:06:34 -0500

    adicionando em application.html.erb o full_title para que cada página tenha um título definido por página ou um título geral --> 3. adicionando <% provide(:title, 'Ajuda') %> nas views Home e Ajuda 4. adicionando testes para testar as modificações em ajuda e home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	# Retorna o título completo para cada página
+	def full_title(page_title)
+		base_title = "Controle de Estoque"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
+	
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,19 +3,21 @@ require 'spec_helper'
 describe "StaticPages" do
 
   describe "Home Page - StaticPages#home" do
-    it "deve ter o conteúdo 'Início'" do
+    it "deve conter no título 'Página Inicial'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       # get static_pages_index_path
       # response.status.should be(200)
     	visit '/static_pages/home'
-    	expect(page).to have_content('Início')
+    	page.should have_title("Controle de Estoque")
     end
 
-    it "deve ter o Título ControleDeEstoque (somente sem o \"|\")" do
-    	visit static_pages_home_path
-    	# Capybara.exact = true
-    	# expect(page).to have_selector("title", :text => 'ControleDeEstoque')
-    	page.should have_selector("title", :exact => 'ControleDeEstoque', :visible => false)
+  end
+
+  describe "Página Ajuda  - static_pages/ajuda" do
+    it "deve ter o conteúdo Ajuda" do
+      visit '/static_pages/ajuda'
+      expect(page).to have_content('Ajuda')
     end
+
   end
 end
